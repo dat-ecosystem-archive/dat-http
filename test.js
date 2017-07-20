90bf1664e73904bc4c87338a459ede7c59603354
Max Ogden <max@maxogden.com>
2017-07-19 17:35:56 -0700

    refactor test

--- test.js
+++ test.js
@@ -10,39 +10,47 @@ var ecstatic = require('ecstatic')
 var datHttp = require('./')
 
 tape('replicate file', function (t) {
+  makeTestServer(t, function runTest (datDir, destDir, cleanup) {
+    var storage = datHttp('http://localhost:9988')
+    var httpDrive = hyperdrive(storage, {latest: true})
+    httpDrive.on('ready', function () {
+      Dat(destDir, {key: httpDrive.key}, function (err, dat) {
+        if (err) return t.ifErr(err, 'error')
+        var localReplicate = dat.archive.replicate()
+        localReplicate.pipe(httpDrive.replicate()).pipe(localReplicate)
+        dat.archive.readFile('/hello.txt', function (err, content) {
+          if (err) return t.ifErr(err, 'error')
+          t.equals(content.toString(), 'hello', 'got hello')
+          t.equals(fs.readFileSync(path.join(destDir, 'hello.txt')).toString(), 'hello', 'file exists and matches')
+          cleanup()
+        })
+      })
+    })    
+  })
+})
+
+function makeTestServer (t, cb) {
   tmpDat(t, function (err, datDir, datCleanup) {
     if (err) t.ifErr(err)
-    var server = http.createServer(ecstatic({ root: datDir }))
-    server.listen(9988, function () {
-      var storage = datHttp('http://localhost:9988')
-      var httpDrive = hyperdrive(storage, {latest: true})
-      httpDrive.on('ready', function () {
-        tmp(function (err, dir, cleanup) {
-          if (err) t.ifErr(err)
-          Dat(dir, {key: httpDrive.key}, function (err, dat) {
-            if (err) return t.ifErr(err, 'error')
-            var localReplicate = dat.archive.replicate()
-            localReplicate.pipe(httpDrive.replicate()).pipe(localReplicate)
-            dat.archive.readFile('/hello.txt', function (err, content) {
-              console.log('read')
-              if (err) return t.ifErr(err, 'error')
-              t.equals(content.toString(), 'hello', 'got hello')
-              t.equals(fs.readFileSync(path.join(dir, 'hello.txt')).toString(), 'hello', 'file exists and matches')
-              cleanup(function (err) {
-                if (err) t.ifErr(err)
-                datCleanup(function (err) {
-                  if (err) t.ifErr(err)
-                  server.close()
-                  t.end()
-                })
-              })
+    tmp(function (err, destDir, tmpCleanup) {
+      if (err) t.ifErr(err)
+      var server = http.createServer(ecstatic({ root: datDir }))
+      server.listen(9988, function () {
+        var cleanup = function () {
+          tmpCleanup(function (err) {
+            if (err) t.ifErr(err)
+            datCleanup(function (err) {
+              if (err) t.ifErr(err)
+              server.close()
+              t.end()
             })
           })
-        })
+        }
+        cb(datDir, destDir, cleanup)
       })
     })
   })
-})
+}
 
 function tmpDat (t, cb) {
   tmp(function created (err, dir, cleanup) {
